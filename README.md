bcef3615d31c439c6a8ab21f83bcd05c7c2dc3b7
naoa <visionary.incident@gmail.com>
2014-08-18 19:28:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,206 @@
+# Yet another delimited tokenizer plugin for Groonga
+
+## Tokenizer
+
+### TokenYaDelimitCaret
+
+It tokenize by splliting with a caret ``^``.
+
+```
+> tokenize TokenYaDelimitCaret "a^b"
+[[0,1408307554.89682,0.00087738037109375],
+[
+{"value":"a","position":0},
+{"value":"b","position":1}
+]]
+```
+
+### TokenYaDelimitColon
+
+It tokenize by splliting with a colon ``:``.
+
+```
+> tokenize TokenYaDelimitColon "a:b"
+[[0,1408307603.1058,0.000877618789672852],
+[
+{"value":"a","position":0},
+{"value":"b","position":1}
+]]
+```
+
+### TokenYaDelimitSemicolon
+
+It tokenize by splliting with a semicolon ``;``.
+
+```
+> tokenize TokenYaDelimitSemicolon "a;b"
+[[0,1408307632.20784,0.000780344009399414],
+[
+{"value":"a","position":0},
+{"value":"b","position":1}
+]]
+```
+
+### TokenYaDelimitPunct
+
+It tokenize by splliting with punctuations ``!"#$%&'()*+,-./:;<=>?@[\]^_`{|}``.
+
+```
+> tokenize TokenYaDelimitPunct "a@b"
+[[0,1408307668.55782,0.00076603889465332],
+[
+{"value":"a","position":0},
+{"value":"b","position":1}
+]]
+```
+
+### TokenYaDelimitPunctWithBlank
+
+It tokenize by splliting with punctuations and blank ``!"#$%&'()*+,-./:;<=>?@[\]^_`{|}  ``.
+
+```
+> tokenize TokenYaDelimitPunctWithBlank "This is a pen. Do you understand?"
+[[0,1408307738.93184,0.00175285339355469],
+[
+{"value":"This","position":0},
+{"value":"is","position":1},
+{"value":"a","position":2},
+{"value":"pen","position":3},
+{"value":"Do","position":4},
+{"value":"you","position":5},
+{"value":"understand","position":6}
+]]
+```
+
+## Install
+
+Install ``groonga-tokenizer-yadelimit`` package:
+
+### CentOS
+
+* CentOS6
+
+```
+% sudo yum localinstall -y http://github.com/naoa/groonga-tokenizer-yadelimit/public/centos/6/groonga-tokenizer-yadelimit-1.0.0-1.el6.x86_64.rpm
+```
+
+* CentOS7
+
+```
+% sudo yum localinstall -y http://github.com/naoa/groonga-tokenizer-yadelimit/public/centos/7/groonga-tokenizer-yadelimit-1.0.0-1.el7.x86_64.rpm
+```
+
+### Fedora
+
+* Fedora 20
+
+```
+% sudo yum localinstall -y http://github.com/naoa/groonga-tokenizer-yadelimit/public/fedora/20/groonga-tokenizer-yadelimit-1.0.0-1.fc20.x86_64.rpm
+```
+
+### Debian GNU/LINUX
+
+* wheezy
+
+```
+% wget http://github.com/naoa/groonga-tokenizer-yadelimit/public/debian/wheezy/groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+% sudo dpkg -i groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+```
+
+* jessie
+
+```
+% wget http://github.com/naoa/groonga-tokenizer-yadelimit/public/debian/jessie/groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+% sudo dpkg -i groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+```
+
+
+### Ubuntu
+
+* precise
+
+```
+% wget http://github.com/naoa/groonga-tokenizer-yadelimit/public/ubuntu/precise/groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+% sudo dpkg -i groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+```
+
+* trusty
+
+```
+% wget http://github.com/naoa/groonga-tokenizer-yadelimit/public/ubuntu/trusty/groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+% sudo dpkg -i groonga-tokenizer-yadelimit_1.0.0-1_amd64.deb
+```
+
+### Source install
+
+Build this tokenizer.
+
+    % sh autogen.sh
+    % ./configure
+    % make
+    % sudo make install
+
+## Dependencies
+
+* Groonga >= 4.0.5
+
+Install ``groonga-devel`` in CentOS/Fedora. Install ``libgroonga-dev`` in Debian/Ubutu.
+
+See http://groonga.org/docs/install.html
+
+## Usage
+
+Firstly, register `tokenizers/yadelimit`
+
+Groonga:
+
+    % groonga db
+    > register tokenizers/yadelimit
+    > table_create Diaries TABLE_HASH_KEY INT32
+    > column_create Diaries body COLUMN_SCALAR TEXT
+    > table_create Terms TABLE_PAT_KEY ShortText --default_tokenizer TokenYaDelimitPunctWithBlank
+    > column_create Terms diaries_body COLUMN_INDEX|WITH_POSITION Diaries body
+
+Mroonga:
+
+    mysql> use db;
+    mysql> select mroonga_command("register tokenizers/yadelimit");
+    mysql> CREATE TABLE `Diaries` (
+        -> id INT NOT NULL,
+        -> body TEXT NOT NULL,
+        -> PRIMARY KEY id(id) USING HASH,
+        -> FULLTEXT INDEX body(body) COMMENT 'parser "TokenYaDelimitPunctWithBlank"'
+        -> ) ENGINE=mroonga DEFAULT CHARSET=utf8;
+
+Rroonga:
+
+    irb --simple-prompt -rubygems -rgroonga
+    >> Groonga::Context.default_options = {:encoding => :utf8}   
+    >> Groonga::Database.create(:path => "/tmp/db")
+    >> Groonga::Plugin.register(:path => "/usr/lib/groonga/plugins/tokenizers/yadelimit.so")
+    >> Groonga::Schema.create_table("Diaries",
+    ?>                              :type => :hash,
+    ?>                              :key_type => :integer32) do |table|
+    ?>   table.text("body")
+    >> end
+    >> Groonga::Schema.create_table("Terms",
+    ?>                              :type => :patricia_trie,
+    ?>                              :normalizer => :NormalizerAuto,
+    ?>                              :default_tokenizer => "TokenYaDelimitPunctWithBlank") do |table|
+    ?>   table.index("Diaries.body")
+    >> end
+    
+## Author
+
+* Naoya Murakami <naoya@createfield.com>
+
+## License
+
+LGPL 2.1. See COPYING for details.
+
+This program includes the Groonga delimited tokenizer.  
+https://github.com/groonga/groonga/blob/master/lib/token.c
+
+This program is the same license as Groonga.
+
+The Groonga delimited tokenizer is povided by ``Copyright(C) 2009-2012 Brazil``
---
+++ tokenizers/yadelimit.c
@@ -0,0 +1,257 @@
+/*
+  Copyright(C) 2014 Naoya Murakami <naoya@createfield.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301  USA
+
+  This file includes the Groonga delimited tokenizer code.
+  https://github.com/groonga/groonga/blob/master/lib/token.c
+
+  The following is the header of the file:
+
+    Copyright(C) 2009-2012 Brazil
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License version 2.1 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
+
+#include <groonga/tokenizer.h>
+
+#include <string.h>
+#include <ctype.h>
+
+#ifdef __GNUC__
+#  define GNUC_UNUSED __attribute__((__unused__))
+#else
+#  define GNUC_UNUSED
+#endif
+
+typedef struct {
+  const unsigned short *delimiter;
+  unsigned int delimiter_len;
+  grn_bool delimit_punct;
+  const unsigned char *next;
+  const unsigned char *end;
+  grn_tokenizer_token token;
+  grn_tokenizer_query *query;
+  grn_bool have_tokenized_delimiter;
+} grn_ya_delimited_tokenizer;
+
+
+static grn_obj *
+delimited_init(grn_ctx *ctx, int nargs, grn_obj **args, grn_user_data *user_data,
+               const unsigned short *delimiter, unsigned int delimiter_len,
+               grn_bool delimit_punct)
+{
+  grn_tokenizer_query *query;
+  unsigned int normalize_flags = 0;
+  const char *normalized;
+  unsigned int normalized_length_in_bytes;
+  grn_ya_delimited_tokenizer *tokenizer;
+
+  query = grn_tokenizer_query_open(ctx, nargs, args, normalize_flags);
+  if (!query) {
+    return NULL;
+  }
+
+  if (!(tokenizer = GRN_PLUGIN_MALLOC(ctx,sizeof(grn_ya_delimited_tokenizer)))) {
+    GRN_PLUGIN_ERROR(ctx,GRN_NO_MEMORY_AVAILABLE,
+        "[tokenizer][yadelimit] "
+        "memory allocation to grn_ya_delimited_tokenizer failed");
+    grn_tokenizer_query_close(ctx, query);
+    return NULL;
+  }
+  user_data->ptr = tokenizer;
+  tokenizer->query = query;
+
+  tokenizer->have_tokenized_delimiter =
+    grn_tokenizer_have_tokenized_delimiter(ctx,
+                                           tokenizer->query->ptr,
+                                           tokenizer->query->length,
+                                           tokenizer->query->encoding);
+  tokenizer->delimiter = delimiter;
+  tokenizer->delimiter_len = delimiter_len;
+  tokenizer->delimit_punct = delimit_punct;
+  grn_string_get_normalized(ctx, tokenizer->query->normalized_query,
+                            &normalized, &normalized_length_in_bytes,
+                            NULL);
+  tokenizer->next = (const unsigned char *)normalized;
+  tokenizer->end = tokenizer->next + normalized_length_in_bytes;
+
+  grn_tokenizer_token_init(ctx, &(tokenizer->token));
+
+  return NULL;
+}
+
+static grn_obj *
+delimited_next(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
+               grn_user_data *user_data)
+{
+  grn_ya_delimited_tokenizer *tokenizer = user_data->ptr;
+
+  if (tokenizer->have_tokenized_delimiter) {
+    unsigned int rest_length;
+    rest_length = tokenizer->end - tokenizer->next;
+    tokenizer->next =
+      (unsigned char *)grn_tokenizer_tokenized_delimiter_next(
+        ctx,
+        &(tokenizer->token),
+        (const char *)tokenizer->next,
+        rest_length,
+        tokenizer->query->encoding);
+  } else {
+    const unsigned char *token_top = tokenizer->next;
+    const unsigned char *token_cursor;
+    const unsigned char *string_end = tokenizer->end;
+
+    unsigned int char_length;
+    unsigned int rest_length = string_end - token_top;
+
+    grn_tokenizer_status status;
+
+    for (token_cursor = token_top; token_cursor < string_end; token_cursor += char_length) {
+      if (!(char_length = grn_plugin_charlen(ctx, (char *)token_cursor, rest_length,
+                                             tokenizer->query->encoding))) {
+        tokenizer->next = (unsigned char *)string_end;
+        break;
+      }
+      if (tokenizer->delimit_punct == GRN_TRUE){
+        if(ispunct(*token_cursor)){
+          tokenizer->next = token_cursor + char_length;
+          break;
+        }
+      }
+      if (tokenizer->delimiter) {
+        if (token_cursor + tokenizer->delimiter_len <= string_end &&
+            !memcmp(token_cursor, tokenizer->delimiter, tokenizer->delimiter_len)) {
+          tokenizer->next = token_cursor + tokenizer->delimiter_len;
+          break;
+        }
+      }
+    }
+    if (token_cursor == string_end) {
+      status = GRN_TOKENIZER_LAST;
+    } else {
+      status = GRN_TOKENIZER_CONTINUE;
+    }
+
+    if (token_cursor == token_top) {
+      status |= GRN_TOKENIZER_TOKEN_SKIP_WITH_POSITION;
+    }
+
+    grn_tokenizer_token_push(ctx, &(tokenizer->token),
+                             (const char *)token_top, token_cursor - token_top, status);
+  }
+
+  return NULL;
+}
+
+static grn_obj *
+delimited_fin(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
+              grn_user_data *user_data)
+{
+  grn_ya_delimited_tokenizer *tokenizer = user_data->ptr;
+  if (!tokenizer) {
+    return NULL;
+  }
+  grn_tokenizer_query_close(ctx, tokenizer->query);
+  grn_tokenizer_token_fin(ctx, &(tokenizer->token));
+  GRN_PLUGIN_FREE(ctx,tokenizer);
+  return NULL;
+}
+
+static grn_obj *
+delimit_caret_init(grn_ctx *ctx, int nargs, grn_obj **args,
+                   grn_user_data *user_data)
+{
+  static const unsigned short delimiter[1] = {'^'};
+  return delimited_init(ctx, nargs, args, user_data, delimiter, 1, 0);
+}
+
+static grn_obj *
+delimit_colon_init(grn_ctx *ctx, int nargs, grn_obj **args,
+                   grn_user_data *user_data)
+{
+  static const unsigned short delimiter[1] = {':'};
+  return delimited_init(ctx, nargs, args, user_data, delimiter, 1, 0);
+}
+
+static grn_obj *
+delimit_semicolon_init(grn_ctx *ctx, int nargs, grn_obj **args,
+                       grn_user_data *user_data)
+{
+  static const unsigned short delimiter[1] = {';'};
+  return delimited_init(ctx, nargs, args, user_data, delimiter, 1, 0);
+}
+
+static grn_obj *
+delimit_punct_init(grn_ctx *ctx, int nargs, grn_obj **args,
+                   grn_user_data *user_data)
+{
+  return delimited_init(ctx, nargs, args, user_data, NULL, 0, GRN_TRUE);
+}
+
+static grn_obj *
+delimit_punct_with_blank_init(grn_ctx *ctx, int nargs, grn_obj **args,
+                              grn_user_data *user_data)
+{
+  static const unsigned short delimiter[1] = {' '};
+  return delimited_init(ctx, nargs, args, user_data, delimiter, 1, GRN_TRUE);
+}
+
+grn_rc
+GRN_PLUGIN_INIT(grn_ctx *ctx)
+{
+  return ctx->rc;
+}
+
+grn_rc
+GRN_PLUGIN_REGISTER(grn_ctx *ctx)
+{
+  grn_rc rc;
+
+  rc = grn_tokenizer_register(ctx, "TokenYaDelimitCaret", -1,
+                              delimit_caret_init, delimited_next, delimited_fin);
+  rc = grn_tokenizer_register(ctx, "TokenYaDelimitColon", -1,
+                              delimit_colon_init, delimited_next, delimited_fin);
+  rc = grn_tokenizer_register(ctx, "TokenYaDelimitSemicolon", -1,
+                              delimit_semicolon_init, delimited_next, delimited_fin);
+  rc = grn_tokenizer_register(ctx, "TokenYaDelimitPunct", -1,
+                              delimit_punct_init, delimited_next, delimited_fin);
+  rc = grn_tokenizer_register(ctx, "TokenYaDelimitPunctWithBlank", -1,
+                              delimit_punct_with_blank_init, delimited_next, delimited_fin);
+
+  return rc;
+}
+
+grn_rc
+GRN_PLUGIN_FIN(GNUC_UNUSED grn_ctx *ctx)
+{
+  return GRN_SUCCESS;
+}
